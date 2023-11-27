69814960a3e0a75ab1e616d2d63219acfd747fed
Minh Lu Xuan <minh.luxuanhcmut@hcmut.edu.vn>
2023-11-27 18:30:09 +0700

    Remove all files in master branch

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-    res.render("index");
-    next();
-});
-
-module.exports = router;
